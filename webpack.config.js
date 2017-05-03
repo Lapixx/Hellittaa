0f6e678d98988af8ba58b2e8943433fd52ca6013
Tijn Kersjes <tijn@divbyzero.nl>
2017-05-03 14:06:48 +0200

    Added dev server and React

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const env = process.env.NODE_ENV || 'development';
+const prod = env === 'production';
 
 const src = path.resolve(__dirname, './src');
 const dist = path.resolve(__dirname, './dist');
@@ -6,11 +11,16 @@ const dist = path.resolve(__dirname, './dist');
 module.exports = {
     context: src,
     entry: {
+        vendor: ['babel-polyfill', 'react', 'react-dom', 'redux', 'react-redux'],
         app: './js/index.js'
     },
     output: {
         path: dist,
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+        publicPath: prod ? '' : '/'
+    },
+    devServer: {
+        contentBase: src
     },
     module: {
         rules: [
@@ -18,7 +28,35 @@ module.exports = {
                 test: /\.js$/,
                 exclude: [/node_modules/],
                 use: ['babel-loader'],
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(src, 'index.html'),
+            path: dist,
+            filename: 'index.html',
+        }),
+        new webpack.ProvidePlugin({
+            'React': 'react'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.js'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(env),
+            },
+            'PRODUCTION': JSON.stringify(prod),
+        }),
+    ]
 };
